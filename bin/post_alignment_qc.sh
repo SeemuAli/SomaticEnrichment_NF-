e51f85bcc1bd391ec9c6f0a026404f0aa6b90052
Seemu Ali <seemu.ali@wales.nhs.uk>
2020-03-12 14:36:30 +0000

    edit file name on mergebam process

--- bin/post_alignment_qc.sh
+++ bin/post_alignment_qc.sh
@@ -13,45 +13,42 @@ vendorPrimaryBed=$6
 padding=$7
 minBQS=$8
 minMQS=$9
+reference_genome=$10
+refdict=$11
 
 #Convert capture BED to interval_list for later
-/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g \
-    -jar /share/apps/picard-tools-distros/picard-tools-2.18.5/picard.jar BedToIntervalList \
+picard BedToIntervalList \
     I=$vendorCaptureBed \
     O="$panel"_capture.interval_list \
-    SD=/state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.dict
+    SD=$refdict
 
 #Convert primary BED to interval_list for later
-/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g \
-    -jar /share/apps/picard-tools-distros/picard-tools-2.18.5/picard.jar BedToIntervalList \
+picard BedToIntervalList \
     I=$vendorPrimaryBed \
     O="$panel"_primary.interval_list \
-    SD=/state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.dict
+    SD=$refdict
 
 #Alignment metrics: library sequence similarity
-/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g \
-    -jar /share/apps/picard-tools-distros/picard-tools-2.18.5/picard.jar CollectAlignmentSummaryMetrics \
-    R=/state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
+picard CollectAlignmentSummaryMetrics \
+    R=$reference_genome \
     I="$seqId"_"$sampleId".bam \
     O="$seqId"_"$sampleId"_AlignmentSummaryMetrics.txt \
     MAX_RECORDS_IN_RAM=2000000 \
-    TMP_DIR=/state/partition1/tmpdir
+    TMP_DIR= . 
 
 #Calculate insert size: fragmentation performance
-/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g \
-    -jar /share/apps/picard-tools-distros/picard-tools-2.18.5/picard.jar CollectInsertSizeMetrics \
+picard CollectInsertSizeMetrics \
     I="$seqId"_"$sampleId".bam \
     O="$seqId"_"$sampleId"_InsertMetrics.txt \
     H="$seqId"_"$sampleId"_InsertMetrics.pdf \
     MAX_RECORDS_IN_RAM=2000000 \
-    TMP_DIR=/state/partition1/tmpdir
+    TMP_DIR= .
 
 #HsMetrics: capture & pooling performance
-/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx2g \
-     -jar /share/apps/picard-tools-distros/picard-tools-2.18.5/picard.jar CollectHsMetrics \
+picard.jar CollectHsMetrics \
      I="$seqId"_"$sampleId".bam \
      O="$seqId"_"$sampleId"_HsMetrics.txt \
-     R=/state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
+     R=$reference_genome \
      BAIT_INTERVALS="$panel"_capture.interval_list \
      TARGET_INTERVALS="$panel"_primary.interval_list \
      MAX_RECORDS_IN_RAM=2000000 \
